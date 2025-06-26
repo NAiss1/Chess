cf5b9d3847e07da25be05cc4236cce9f4e23ded9
NAiss1 <146001880+NAiss1@users.noreply.github.com>
2025-06-26 12:06:33 +0100

    Add files via upload

--- src/gui/ChessBoard.java
+++ src/gui/ChessBoard.java
@@ -1,1 +1,17 @@
 
+// gui/ChessBoard.java
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessBoard extends JFrame {
+    public ChessBoard() {
+        setTitle("Java Chess");
+        setSize(800, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        add(new ChessPanel());
+        setVisible(true);
+    }
+}
